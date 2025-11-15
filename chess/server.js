089370d39214ee505dc96f3488c7e17720033507
Pathum-Vimukthi-Kumara <your-email@example.com>
2025-11-15 11:58:02 +0530

    change to 0.0.0.0

--- chess/server.js
+++ chess/server.js
@@ -862,7 +862,7 @@ app.get('*', (req, res) => {
 })
 
 const port = process.env.PORT || 3000
-const host = process.env.HOST || '192.168.8.102'
+const host = process.env.HOST || '0.0.0.0'
 
 httpServer.listen(port, host, () => {
     console.log(`> Server listening on http://${host}:${port}`)
